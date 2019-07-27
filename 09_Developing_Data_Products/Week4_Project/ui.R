0bb0552492b0b04b0d938c2bace5c09644913477
Oliver Stroemer <15880916+olistroemer@users.noreply.github.com>
2019-07-27 04:42:45 +0200

    09/04 link to github

--- 09_Developing_Data_Products/Week4_Project/ui.R
+++ 09_Developing_Data_Products/Week4_Project/ui.R
@@ -14,7 +14,10 @@ shinyUI(fluidPage(
             p("Using the drop-down fields you can assign the different variables to the features of the plot. The plot will change immediately."),
             h3("Regression Line"),
             p("In the plot you can see a regression line with the 95 % confidence interval around it. It describes the relationship between the regressand and the regressor."),
-            p("You can also hide the line using the checkbox.")
+            p("You can also hide the line using the checkbox."),
+            h3("Source Code"),
+            p("You can find the source code of this application here:"),
+            a("GitHub link", href="https://github.com/olistroemer/datasciencecoursera/tree/master/09_Developing_Data_Products/Week4_Project")
         ),
         mainPanel(
             plotOutput("plot")
